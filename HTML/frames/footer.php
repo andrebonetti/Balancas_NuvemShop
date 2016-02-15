c8af5f3a49346855c10dcec09564675ca52fc255
andrebonetti <andrebonetti2@gmail.com>
2016-02-15 20:51:26 -0200

    Update 15/02/2016
    atualizado pra PHP
    Página Sobre

---
+++ HTML/frames/footer.php
@@ -0,0 +1,96 @@
+        <footer>
+            
+            <div class="myContainer">
+            
+                <div>
+            
+                    <h2>Informações</h2>
+                    
+                    <div class="lista">
+                        
+                        <a href="#">Sobre Nós</a>
+                        <a href="#">Informações sobre Entrega</a>
+                        <a href="#">Politica de Privacidade</a>
+                        <a href="#">Termos e Condições</a>
+                        <a href="#">Blog</a>
+                        
+                    </div>    
+
+                    <h2>Conta</h2>
+                    
+                    <div class="lista">
+                        
+                        <a href="#">Minha Conta</a>
+                        <a href="#">Favoritos</a>
+                        <a href="#">Histórico de Compras</a>
+                        
+                    </div>    
+                
+                </div>
+            
+                <div>
+
+                    <h2>Categorias</h2>
+                    
+                    <div class="lista">
+                        
+                        <a href="#">Amaciadores de Carne</a>
+                        <a href="#">Balanças</a>
+                        <a href="#">Batedeiras</a>
+                        <a href="#">Cafeteiras</a>
+                        <a href="#">Cortadores de Frios</a>	
+                        <a href="#">Cutters</a>	
+                        <a href="#">Extratores de Suco</a>	
+                        <a href="#">Fatiadores de Carne</a>
+                        <a href="#">Fritadeiras Elétricas</a>
+                        <a href="#">Liquidificadores</a>	
+                        <a href="#">Moedores de Carne</a>
+                        <a href="#">Processadores de Alimento</a>	
+                        <a href="#">Raladores de Queijo</a>
+                        <a href="#">Serras</a>
+                        
+                    </div>
+                    
+                </div>
+
+                <div>
+
+                    <h2>Contato</h2>
+                    
+                    <div class="lista">
+                        
+                        <h3>Endereço</h3>
+                        
+                        <p>Av. Itinguçu, 2485</p>
+                        <p>Penha</p>
+                        <p>São Paulo, SP</p>
+
+                        <h3>Telefones</h3>
+                        
+                        <p>(11) 2023-1723</p>                
+                        <p>(11) 2682-2830</p>
+
+                        <h3>E-mail</h3>
+                        
+                        <p>bonettisilveira@terra.com.br</p> 
+                        
+                    </div>
+                    
+                    <h2>Redes Sociais</h2>
+                    
+                    <div class="lista">
+                        
+                        <a href="#">Facebook</a>
+                        <a href="#">Instagran</a>
+                        <a href="#">Twitter</a>
+                        
+                    </div>    
+
+                </div>
+                
+            </div>    
+            
+        </footer>
+
+    </body>
+</html> 

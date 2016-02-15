bf0da660ae724568b2d73b9bfb8b241da39264e7
andrebonetti <andrebonetti2@gmail.com>
2016-02-15 21:30:35 -0200

    Update 15/02/2016
    Página Serviços
    Modal Contato
    
    Framde NAV

--- HTML/index.php
+++ HTML/index.php
@@ -4,28 +4,7 @@
             
         </section> 
         
-        <div class="menu-principal">
-            
-            <div class="myContainer">
-                
-                <nav>
-                    <a href="index.php" class="active">Home</a>
-                    <a href="sobre.php">Empresa</a>
-                    <a href="servicos.php">Serviços</a>
-                    <a href="produtos.php">Produtos</a>
-                </nav>
-
-                <div class="categorias">
-                    <a href="">Balanças</a>
-                    <a href="">Cortadores de Frios</a>
-                    <a href="">Liquidificadores</a>
-                    <a href="">Serras</a>
-                    <a href="">Mais +</a>
-                </div>
-                
-            </div>    
-            
-        </div>
+        <?php include_once("frames/nav.php")?>
             
         <section class="home">
             
--- HTML/sobre.php
+++ HTML/sobre.php
@@ -1,27 +1,6 @@
 <?php include_once("frames/header.php")?>
         
-        <div class="menu-principal">
-            
-            <div class="myContainer">
-                
-                <nav>
-                    <a href="index.php">Home</a>
-                    <a href="sobre.php" class="active">Empresa</a>
-                    <a href="servicos.php">Serviços</a>
-                    <a href="produtos.php">Produtos</a>
-                </nav>
-
-                <div class="categorias">
-                    <a href="">Balanças</a>
-                    <a href="">Cortadores de Frios</a>
-                    <a href="">Liquidificadores</a>
-                    <a href="">Serras</a>
-                    <a href="">Mais +</a>
-                </div>
-                
-            </div>    
-            
-        </div>
+<?php include_once("frames/nav.php")?>
             
         <section class="sobre">
             
